b7a3595b5bc64617a4295768700682f5aafc406e
liaokt <liaokt26196@yunrong.cn>
2024-04-30 10:16:26 +0800

    feat: '修改404页面布局以及添加返回按钮'

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,18 +3,38 @@
  * @Description: not-found-page
  * @Date: 2024-04-29 15:52:59
  * @LastEditors: liaokt
- * @LastEditTime: 2024-04-29 17:54:14
+ * @LastEditTime: 2024-04-30 10:15:44
  */
 
+"use client";
+
 import { Button } from "antd";
-import { ArrowLeftOutlined } from "@ant-design/icons";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 import "./globals.css";
 
-// TODO: 背景中的图片和文字都要单独拿出来做布局
 const NotFoundPage = () => {
+  const router = useRouter();
+
+  const onBack = () => {
+    router.back();
+  };
+
   return (
     <div className={"not_found"}>
-      <Button icon={<ArrowLeftOutlined />} shape={"round"} type={"primary"} style={{ marginLeft: "2.8vw" }}>
+      <div className={"img_404"}>
+        <Image src="/imgs/img_404.png" alt="404 Not Found" layout="fill" objectFit="contain" />
+      </div>
+      <div className={"text_404"}>
+        <Image src="/imgs/text_404.png" alt="404 Not Found" layout="fill" objectFit="contain" />
+      </div>
+      <Button
+        onClick={() => {
+          onBack();
+        }}
+        className={"button_404"}
+        shape={"round"}
+        type={"primary"}>
         {"BACK TO LAST PAGE"}
       </Button>
     </div>
